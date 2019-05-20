908e2a3ce8840242d4f65b4e1ecff30e739e66eb
Neurosyphilis <50843298+Neurosyphilis@users.noreply.github.com>
2019-05-20 19:45:42 +0000

    Add files via upload

---
+++ arch3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cfdisk /dev/sda
+mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+swapon /dev/sda2
+mkfs.ext4 /dev/sda3
+mount /dev/sda3 /mnt
+mkdir /mnt/boot /mnt/home /mnt/var
+mount /dev/sda1 /mnt/boot
+vim /etc/pacman.d/mirrorlist
+pacstrap /mnt base base-devel
+pacstrap /mnt grub-bios
+genfstab -p /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+hwclock --systohc --utc
+mkinitcpio -p linux
+passwd root
+read -p "Username : " username
+useradd -mg users -G wheel -s /bin/bash $username
+passwd $username
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+vim /etc/sudoers
+exit
+sudo reboot now
