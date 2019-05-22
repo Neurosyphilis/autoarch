02fab929229c62ba1d9421c385a11b148e30d742
Neurosyphilis <50843298+Neurosyphilis@users.noreply.github.com>
2019-05-22 20:04:58 +0300

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -1,26 +1,31 @@
-#!/bin/bash
+!#/bin/bash
 cfdisk /dev/sda
 mkfs.ext4 /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
 mkfs.ext4 /dev/sda3
 mount /dev/sda3 /mnt
-mkdir /mnt/boot /mnt/home /mnt/var
+mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel grub-bios
-genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
+genfstab -U /mnt >> /mnt/etc/fstab
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
 hwclock --systohc --utc
+mkinitcpio -p linux
 passwd root
 read -p "Username : " username
-read -p "Hostname : " hostname
 useradd -mg users -G wheel -s /bin/bash $username
 passwd $username
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 vim /etc/sudoers
-hostnamectl set-hostname $hostname
-systemctl enable dhcpcd
+hwclock --systohc --utc
+mkinitcpio -p linux
+passwd root
+read -p "Username: " username
+useradd -mg users -G wheel -s /bin/bash $username
+passwd $username
 exit
-sudo reboot now
